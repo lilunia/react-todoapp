752dc123f78d88cb780d2393b7b414e45dcc8e43
Liliana <lilkupczyk@gmail.com>
2024-01-21 19:57:08 +0100

    modified h1

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
 		<div className={styles.container}>
 			<header className={styles.header}>
 				<div>
-					<h1>Do zrobienia</h1>
+					<h1>Todo</h1>
 					<h2>{getSubheading(todos.length)}</h2>
 				</div>
 				{!isFormShown && (
